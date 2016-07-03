ba6e540ef89f1dd4748957b6015434e802986428
thijvb <thijsvb@gmail.com>
2016-07-03 22:49:30 +0200

    exported android pd project

---
+++ android/src/processing/test/cavex/cavex.java
@@ -0,0 +1,444 @@
+package processing.test.cavex;
+
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import ketai.ui.*; 
+import android.view.MotionEvent; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class cavex extends PApplet {
+
+
+
+
+KetaiGesture gesture;
+
+int n;
+int levelScore;
+int killScore;
+boolean[] out;
+boolean[] mouseOver;
+ship[] ships = new ship[4];
+boolean gDisk, bDisk;
+float gDiskO, bDiskO;
+int[] boomLine = {};
+float[] boomDist = {};
+float[] boomSize = {};
+PFont f;
+PImage guideImg;
+boolean guide = false;
+
+public void setup() {
+  
+  orientation(PORTRAIT);
+  textAlign(CENTER, CENTER);
+  f = createFont("Trebuchet_MS.ttf", 20);
+  textFont(f);
+  strokeWeight(3);
+  n = 6;
+  out = new boolean[n];
+  for (int i=0; i!=n; ++i) {
+    out[i] = true;
+  }
+  mouseOver = new boolean[n];
+  levelScore = 0;
+  killScore = 0;
+  for (int i=0; i!=4; ++i) {
+    float g = random(3);
+    float m = random(5);
+    ships[i] = new ship((int)(random(0, n)), g>=2, m>=4, width/2 + i*width/3);
+  }
+
+  guideImg = loadImage("guide.png");
+  guideImg.resize(width, width);
+  gesture = new KetaiGesture(this);
+}
+
+public void draw() {
+  if (guide) {
+    showGuide();
+    return;
+  }
+  translate(width/2, height/2);
+  background(32);
+  //Field
+  fill(0);
+  noStroke();
+  ellipse(0, 0, width, width);
+  stroke(32);
+  for (int i=0; i!=n; ++i) {
+    float a = i*TWO_PI/n;
+    line(0, 0, cos(a)*width/2, sin(a)*width/2);
+  }
+  noFill();
+  ellipse(0, 0, width/10, width/10);
+  ellipse(0, 0, width/3, width/3);
+  //Kill and level meters
+  noStroke();
+  fill(255, 0, 0);
+  arc(-width/2+width/8+10, -height/2+width/8+10, width/4, width/4, -PI/2, killScore*TWO_PI/n - PI/2);
+  fill(0, 255, 0);
+  arc(width/2-width/8-10, -height/2+width/8+10, width/4, width/4, -PI/2, levelScore*TWO_PI/n - PI/2);
+  noFill();
+  stroke(255);
+  ellipse(-width/2+width/8+10, -height/2+width/8+10, width/4, width/4);
+  ellipse(width/2-width/8-10, -height/2+width/8+10, width/4, width/4);
+  //Level indicator
+  fill(255);
+  textSize(width*0.15f);
+  text("lvl: " + (n-6), 0, height/2-width/4);
+  textSize(width/20);
+  text("help", -width/2+width/15, height/2-width/20);
+  //Player
+  drawPoly();
+  //Docking disks
+  noStroke();
+  if (gDisk) {
+    if (gDiskO < 20) {
+      ++gDiskO;
+      fill(0, 255, 0, map(gDiskO, 0, 20, 255, 0));
+      ellipse(0, 0, width/10, width/10);
+    } else {
+      gDiskO = 0;
+      gDisk = false;
+    }
+  }
+  if (bDisk) {
+    if (bDiskO < 20) {
+      ++bDiskO;
+      fill(255, 0, 0, map(bDiskO, 0, 20, 255, 0));
+      ellipse(0, 0, width/10, width/10);
+    } else {
+      bDiskO = 0;
+      bDisk = false;
+    }
+  }
+  //BOOMS!
+  fill(255, 192, 0);
+  stroke(255, 0, 0);
+  if (boomLine.length > 0) {
+    for (int i=0; i!=boomLine.length; ++i) {
+      boomSize[i]+=width/300;
+      ellipse(cos(boomLine[i]*TWO_PI/n)*boomDist[i], sin(boomLine[i]*TWO_PI/n)*boomDist[i], boomSize[i], boomSize[i]);
+    }
+    if (boomSize[0] >= width/20) {
+      boomLine = subset(boomLine, 1);
+      boomDist = subset(boomDist, 1);
+      boomSize = subset(boomSize, 1);
+    }
+  }
+  //Mouse indicators
+  noStroke();
+  fill(0, 255, 255, 128);
+  for (int i=0; i!=n; ++i) {
+    float a = i*TWO_PI/n;
+    if (mouseOver[i] && out[i]) {
+      ellipse(cos(a)*width/6, sin(a)*width/6, width/60, width/60);
+    } else if (mouseOver[i] && !out[i]) {
+      ellipse(cos(a)*width/20, sin(a)*width/20, width/60, width/60);
+    }
+  }
+  //Ships
+  for (int i=0; i!=4; ++i) {
+    ships[i].move();
+    if (ships[i].dist < width/2) {
+      ships[i].display();
+    }
+    //Collision
+    if (ships[i].good && ships[i].dist <= width/6 && out[ships[i].line]) {    //good ship at out vertex will never result in a point
+      boomLine = append(boomLine, ships[i].line);
+      boomDist = append(boomDist, width/6);
+      boomSize = append(boomSize, 0);
+      resetShip(i);
+    }
+
+    if (ships[i].good && ships[i].dist <= width/20) {                          //good ship at in vertex will result in a point if the angle is concave
+      PVector a = new PVector(cos(ships[i].line*TWO_PI/n), sin(ships[i].line*TWO_PI/n));
+      if (out[ships[i].line]) {
+        a.setMag(width/6);
+      } else {
+        a.setMag(width/20);
+      }
+      PVector b = new PVector(cos(((ships[i].line+1)%n) *TWO_PI/n), sin(((ships[i].line+1)%n) *TWO_PI/n));
+      if (out[(ships[i].line+1)%n]) {
+        b.setMag(width/6);
+      } else {
+        b.setMag(width/20);
+      }
+      PVector c = new PVector(cos(((ships[i].line+(n-1))%n) *TWO_PI/n), sin(((ships[i].line+(n-1))%n) *TWO_PI/n));
+      if (out[(ships[i].line+(n-1))%n]) {
+        c.setMag(width/6);
+      } else {
+        c.setMag(width/20);
+      }
+
+      PVector d;
+      d = PVector.sub(b, a);
+      PVector e;
+      e = PVector.sub(c, a);
+      if (realAngleBetween(d, e) < PI) {    //because PVector.angleBetween() always gives the smallest angle
+        if (ships[i].master) {
+          levelScore = n;
+        } else {
+          ++levelScore;
+        }
+        gDisk = true;
+      } else {
+        boomLine = append(boomLine, ships[i].line);
+        boomDist = append(boomDist, width/20);
+        boomSize = append(boomSize, 0);
+      }
+
+      resetShip(i);
+    }
+
+    if (!ships[i].good && ships[i].dist <= width/6 && out[ships[i].line]) {    //bad ship at out vertex will never result in a kill point
+      boomLine = append(boomLine, ships[i].line);
+      boomDist = append(boomDist, width/6);
+      boomSize = append(boomSize, 0);
+      resetShip(i);
+    }
+
+    if (!ships[i].good && ships[i].dist <= width/20) {      //bad ship at in vertex will only result in kill point if the angle is concave
+      PVector a = new PVector(cos(ships[i].line*TWO_PI/n), sin(ships[i].line*TWO_PI/n));
+      if (out[ships[i].line]) {
+        a.setMag(width/6);
+      } else {
+        a.setMag(width/20);
+      }
+      PVector b = new PVector(cos(((ships[i].line+1)%n) *TWO_PI/n), sin(((ships[i].line+1)%n) *TWO_PI/n));
+      if (out[(ships[i].line+1)%n]) {
+        b.setMag(width/6);
+      } else {
+        b.setMag(width/20);
+      }
+      PVector c = new PVector(cos(((ships[i].line+(n-1))%n) *TWO_PI/n), sin(((ships[i].line+(n-1))%n) *TWO_PI/n));
+      if (out[(ships[i].line+(n-1))%n]) {
+        c.setMag(width/6);
+      } else {
+        c.setMag(width/20);
+      }
+
+      PVector d;
+      d = PVector.sub(b, a);
+      PVector e;
+      e = PVector.sub(c, a);
+      if (realAngleBetween(d, e) < PI) {    //because PVector.angleBetween() always gives the smallest angle
+        if (ships[i].master) {
+          killScore = n;
+        } else {
+          ++killScore;
+        }
+        bDisk = true;
+      } else {
+        boomLine = append(boomLine, ships[i].line);
+        boomDist = append(boomDist, width/20);
+        boomSize = append(boomSize, 0);
+      }
+
+      resetShip(i);
+    }
+  }
+
+  if (levelScore >= n) {
+    levelScore = 0;
+    ++n;
+    resetArrays();
+  }
+  if (killScore >= n) {
+    killScore = 0;
+    --n;
+    resetArrays();
+  }
+  if (n < 6) {
+    n = 6;
+    resetArrays();
+  }
+}
+
+public void resetArrays() {
+  out = new boolean[n];
+  for (int i=0; i!=n; ++i) {
+    out[i] = true;
+  }
+  mouseOver = new boolean[n];
+  for (int i=0; i!=ships.length; ++i) {
+    if (ships[i].line >= n) {
+      resetShip(i);
+    }
+  }
+}
+
+//Magic Android/Ketai stuff from a book
+public boolean surfaceTouchEvent(MotionEvent event) {
+  super.surfaceTouchEvent(event);
+  return gesture.surfaceTouchEvent(event);
+}
+
+//Now do something when you tap the screen
+public void onTap(float x, float y) {  
+  if (!guide) {
+    for (int i=0; i!=n; ++i) {
+      //Set mouseOver
+      float a = i*TWO_PI/n;
+      PVector m = new PVector(x-width/2, y-height/2);
+      PVector v = new PVector(cos(a), sin(a));
+
+      if (m.mag() <= width/2 && (realAngleBetween(v, m) < PI/n || abs(realAngleBetween(v, m)-TWO_PI) < PI/n)) {
+        mouseOver[i] = true;
+      } else {
+        mouseOver[i] = false;
+      }
+
+      if (mouseOver[i]) {
+        out[i] = !out[i];
+      }
+    }
+  }
+
+  if (dist(x, y, width/15, height-width/20) < width/8) {
+    guide = !guide;
+  }
+}
+
+public void drawPoly() {
+  beginShape();
+  noFill();
+  stroke(255);
+  for (int i=0; i!=n; ++i) {
+    float a = i*TWO_PI/n;
+    float r;
+    if (out[i]) {
+      r = width/6;
+    } else {
+      r = width/20;
+    }
+    vertex(cos(a)*r, sin(a)*r);
+  }
+  endShape(CLOSE);
+}
+
+public float realAngleBetween(PVector a, PVector b) {
+  float aAngle = atan(a.y/a.x);
+  if (a.x<0 && a.y>0) {
+    aAngle=abs(aAngle+PI);
+  }
+  if (a.x<0 && a.y<0) {
+    aAngle+=PI;
+  }
+  if (a.x>0 && a.y<0) {
+    aAngle=abs(aAngle+TWO_PI);
+  }
+
+  float bAngle = atan(b.y/b.x);
+  if (b.x<0 && b.y>0) {
+    bAngle=abs(bAngle+PI);
+  }
+  if (b.x<0 && b.y<0) {
+    bAngle+=PI;
+  }
+  if (b.x>0 && b.y<0) {
+    bAngle=abs(bAngle+TWO_PI);
+  }
+
+  return (aAngle-bAngle+TWO_PI)%TWO_PI;
+}
+
+public void resetShip(int s) {
+  float max = 0;
+  for (int i=0; i!=ships.length; ++i) {
+    if (i!=s && ships[i].dist > max) {
+      max = ships[i].dist;
+    }
+  }
+  if (max < width/2 - width/8) {
+    ships[s].reset(width/2);
+  } else {
+    ships[s].reset(max + width/8);
+  }
+}
+
+class ship {
+  int line;
+  boolean good;
+  boolean master;
+  float dist;
+
+  ship(int lineInput, boolean goodInput, boolean masterInput, float distInput) {
+    line = lineInput;
+    good = goodInput;
+    master = masterInput;
+    dist = distInput;
+  }
+
+  public void display() {
+    if (good) {
+      if (master) {
+        fill(0);
+        stroke(0, 255, 0);
+      } else {
+        noStroke();
+        fill(0, 255, 0);
+      }
+      ellipse(cos(line*TWO_PI/n)*(dist+10), sin(line*TWO_PI/n)*(dist+10), width/30, width/30);
+    } else {
+      if (master) {
+        fill(0);
+        stroke(255, 0, 0);
+      } else {
+        noStroke();
+        fill(255, 0, 0);
+      }
+      triangle(cos(line*TWO_PI/n)*dist, sin(line*TWO_PI/n)*dist, 
+        cos(line*TWO_PI/n)*(dist+width/20) + cos((line*TWO_PI/n)+PI/2)*width/60, sin(line*TWO_PI/n)*(dist+width/20) + sin((line*TWO_PI/n)+PI/2)*width/60, 
+        cos(line*TWO_PI/n)*(dist+width/20) + cos((line*TWO_PI/n)-PI/2)*width/60, sin(line*TWO_PI/n)*(dist+width/20) + sin((line*TWO_PI/n)-PI/2)*width/60);
+    }
+  }
+
+  public void move() {
+    dist -= width/300;
+  }
+
+  public void reset(float d) {
+    float g = random(3);
+    good = g>=2;
+    if (good) {
+      float m = random(30);
+      master = m>=29;
+    } else {
+      float m = random(3);
+      master = m>=2;
+    }
+    line = (int)(random(0, n));
+    dist = d;
+  }
+}
+
+public void showGuide() {
+  background(32);
+  image(guideImg, 0, height/2-width/2);
+  fill(255);
+  textSize(width/20);
+  text("play", width/15, height-width/20);
+}
+  public void settings() {  fullScreen(); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "cavex" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
